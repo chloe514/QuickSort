8305fa7621de6df533b0264333e6a1e4beba8c45
chloe514 <chloevans22@gmail.com>
2024-08-13 09:53:14 -0400

    completed assignment

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        QuickSort sorter = new QuickSort();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int N1 = 5;
+        int[] arr1 = {4, 1, 3, 9, 7};
+        sorter.quickSort(arr1, 0, N1 - 1);
+        printArray(arr1);
+
+        int N2 = 9;
+        int[] arr2 = {2, 1, 6, 10, 4, 1, 3, 9, 7};
+        sorter.quickSort(arr2, 0, N2 - 1);
+        printArray(arr2);
+    }
+
+    // Function to print the array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
         }
+        System.out.println();
     }
 }
